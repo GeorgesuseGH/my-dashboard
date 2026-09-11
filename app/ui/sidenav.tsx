8b06bd651939b167f6621a1d7f8e1c38e5abe839
GeorgesAH <georgesms2ndacc@gmail.com>
2026-09-11 12:24:37 +0300

    fixes

--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -4,26 +4,7 @@ import Image from "next/image";
 export default function SideNav(){
     return (
         <div className="flex flex-col items-between justify-center h-full w-[15%]">
-         <Link href="" className="grid grid-cols-2">
-         <Image width="" height="" alt="" src=""></Image>
-         <span>Invoices
-            </span></Link>
-            <Link href="" className="grid grid-cols-2">
-         <Image width="" height="" alt="" src=""></Image>
-         <span>Customers            
-            </span></Link>
-            <Link href="" className="grid grid-cols-2">
-         <Image width="" height="" alt="" src=""></Image>
-         <span>Goals
-            </span></Link>
-            <Link href="" className="grid grid-cols-2">
-         <Image width="" height="" alt="" src=""></Image>
-         <span>Investments
-            </span></Link>
-            <Link href="" className="grid grid-cols-2">
-         <Image width="" height="" alt="" src=""></Image>
-         <span>Business
-            </span></Link>
+         <div>Some design</div>
         </div>
     )
 }
